4689a79e9fc2bf78a97fcf78b195a6c7d69554f3
Popwers <lionel.bataille@hotmail.com>
2026-01-28 22:13:39 +0400

    fix: guard mac-specific dotfile config on linux

--- .profile
+++ .profile
@@ -1,9 +1,12 @@
-eval "$(/opt/homebrew/bin/brew shellenv)"
+if [ -x "/opt/homebrew/bin/brew" ]; then
+	eval "$(/opt/homebrew/bin/brew shellenv)"
+fi
 
 export OPENCODE_EXPERIMENTAL=true
 
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 
-export PATH="$HOME/.node_modules/bin:$PATH"
+export BUN_INSTALL="${BUN_INSTALL:-$HOME/.bun}"
+export PATH="$HOME/.opencode/bin:$BUN_INSTALL/bin:$HOME/.local/bin:$HOME/.node_modules/bin:$PATH"
 export npm_config_prefix="$HOME/.node_modules"
--- config.fish
+++ config.fish
@@ -6,8 +6,10 @@ set fish_greeting ""
 set -gx TERM xterm-256color
 set -gx OPENCODE_EXPERIMENTAL true
 
-# Setup brew
-eval "$(/opt/homebrew/bin/brew shellenv)"
+# Setup brew (macOS only)
+if test -x /opt/homebrew/bin/brew
+    eval "(/opt/homebrew/bin/brew shellenv)"
+end
 
 # aliases
 alias .. "cd .."
@@ -16,15 +18,22 @@ alias .... "cd ../../.."
 alias ..... "cd ../../../.."
 alias grep "grep --color=auto"
 alias mkdir "mkdir -p"
-alias ls "eza --long --color=always --icons=always --all"
-alias l "eza --long --color=always --icons=always --all"
-alias lss "eza --long --color=always --icons=always --all --total-size --sort=size"
-alias cat bat
+if type -q eza
+    alias ls "eza --long --color=always --icons=always --all"
+    alias l "eza --long --color=always --icons=always --all"
+    alias lss "eza --long --color=always --icons=always --all --total-size --sort=size"
+end
+
+if type -q bat
+    alias cat bat
+end
 alias vim nvim
 alias vi nvim
 alias cz "git cz"
 alias ga "git add . && git cz"
-alias upsys "brew update && brew upgrade && brew cleanup && brew doctor && bun upgrade && bun -g update"
+if type -q brew
+    alias upsys "brew update && brew upgrade && brew cleanup && brew doctor && bun upgrade && bun -g update"
+end
 
 function opencode
     set -l state_dir /tmp/opencode-state
@@ -122,14 +131,19 @@ function opencode
 end
 
 # Path
-fish_add_path /opt/homebrew/bin
-fish_add_path /opt/homebrew/sbin
+if test -d /opt/homebrew/bin
+    fish_add_path /opt/homebrew/bin
+end
+if test -d /opt/homebrew/sbin
+    fish_add_path /opt/homebrew/sbin
+end
 fish_add_path /usr/local/bin
 fish_add_path bin
 fish_add_path ~/bin
 fish_add_path ~/.local/bin
 fish_add_path ~/.orbstack/bin
 fish_add_path ~/.opencode/bin
+fish_add_path ~/.bun/bin
 fish_add_path node_modules/.bin
 
 # NodeJS
@@ -140,4 +154,6 @@ if test "$TERM_PROGRAM" = "vscode" -o "$TERM_PROGRAM" = "cursor"
     . (code --locate-shell-integration-path fish)
 end
 
-oh-my-posh init fish --config (brew --prefix oh-my-posh)/themes/jandedobbeleer.omp.json | source
+if test -f ~/.config/oh-my-posh/jandedobbeleer.omp.json
+    oh-my-posh init fish --config ~/.config/oh-my-posh/jandedobbeleer.omp.json | source
+end
